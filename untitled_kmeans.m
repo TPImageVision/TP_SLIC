da054430a2839efa9841ae0aa37ae290b4146f39
TagZK <julien.davidou@gmail.com>
2023-10-24 14:46:28 +0200

    Fix de l'algo

--- untitled_kmeans.m
+++ untitled_kmeans.m
@@ -16,9 +16,11 @@ for i = 1:nb_images
     im(:,:,:,i) = imread(nom); 
 end
 
-figure;
-imshow(im(:,:,:,1));
-
+figure; 
+subplot(2,2,1); imshow(im(:,:,:,1)); title('Image 1');
+subplot(2,2,2); imshow(im(:,:,:,9)); title('Image 9');
+subplot(2,2,3); imshow(im(:,:,:,17)); title('Image 17');
+subplot(2,2,4); imshow(im(:,:,:,25)); title('Image 25');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % A COMPLETER                                             %
@@ -28,92 +30,104 @@ imshow(im(:,:,:,1));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % ........................................................%
-%% Initialisation
-
-im_1 = double(im(:,:,:,1));
-K = 200;
-[germ,S] = SLIC(im_1,K);
-K = size(germ,1);
-m = 1;
-% coté du voisinage d'un germe
-n = 3;
-
-matrix_label = - ones(size(im_1(:,:,1)));
-matrix_distance = zeros(size(im_1(:,:,1)));
-matrix_distance(:,:) = realmax;
-current_distance = zeros(size(matrix_distance));
-
-[h,w] = size(current_distance);
-
-%% Affinement de la grille
-
-[Gx,Gy] = imgradient(rgb2gray(im(:,:,:,1)));
-grad = (double(Gx)+double(Gy))/2;
-for i = 1:size(germ,1)
-    x_germ = germ(i,1);
-    y_germ = germ(i,2);
-    x_min = max(1,x_germ-1);
-    x_max = min(h,x_germ+1);
-    y_min = max(1,y_germ-1);
-    y_max = min(w,y_germ+1);
-    % Voisinage n du germe
-
-    voisinage = grad(x_min:x_max,y_min:y_max);
-    % Valeur minimale dans le voisinage
-
-    [valeur,indice]=min(voisinage(:));
-    [indX,indY] = ind2sub(size(voisinage),indice);
-    x_newgerm = x_min+(indX-1);
-    y_newgerm = y_min+(indY-1);
-    % Mise à jour du germe
-
-    germ(i,1) = x_newgerm;
-    germ(i,2) = y_newgerm;
-    germ(i,3) = im_1(x_newgerm,y_newgerm,1);
-    germ(i,4) = im_1(x_newgerm,y_newgerm,2);
-    germ(i,5) = im_1(x_newgerm,y_newgerm,3);
-
-end
-
-%% Kmeans
-% Transformation de l'image au format des germes et attenuation
-im_kmeans = zeros(h*w,5);
-ligne=1;
-for x=1:h
-    for y=1:w
-        im_kmeans(ligne,:)=[x*(m/S) y*(m/S) im_1(x,y,1) im_1(x,y,2) im_1(i,y,3)];
-        ligne=ligne+1;
+% Nombre de superpixels recherchés
+K = 400;
+for n = 1:4
+    % Pour aller de 8 en 8 (1,9,17,25)
+    n_calc = ((n-1) * 8)+1;
+    
+    %% Initialisation
+    im_n = double(im(:,:,:,n_calc));
+    % Initialisation des centres
+    %[germ,S] = SLIC(im_1,K);
+    [h,w] = size(im_n(:,:,1));
+    
+    S = sqrt((h*w)/K);
+    
+    [X,Y] = meshgrid(S/2:S:h,S/2:S:w);
+    
+    germ = zeros(size(X,1)*size(X,2),5);
+    
+    germ(:,1) = X(:);
+    germ(:,2) = Y(:);
+    
+    for i=1:size(germ,1)
+        cx = germ(i,1);
+        cy = germ(i,2);
+        rgb = im_n(round(cx),round(cy),:);
+        germ(i,3) = rgb(:,1);
+        germ(i,4) = rgb(:,2);
+        germ(i,5) = rgb(:,3);
     end
-end
-for ligne=1:K
-   germ(ligne,1)=germ(ligne,1)*(m/S);
-   germ(ligne,2)=germ(ligne,2)*(m/S);
-end
-[res_kmeans, new_germ] = kmeans(im_kmeans,K,'Start',germ,'EmptyAction','drop');
-for ligne=1:size(new_germ,1)
-   new_germ(ligne,1)=new_germ(ligne,1)*(S/m);
-   new_germ(ligne,2)=new_germ(ligne,2)*(S/m);
-end
-matrix_label = (reshape(res_kmeans,size(im_1,2),size(im_1,1)))';
-figure;
-matrix_segmentation = classe2segmentation(matrix_label);
-imshow(matrix_segmentation);
-
-%% Binarisation
-
-% Initialisation
-mat_binarisation = zeros(size(im_1(:,:,1)));
-mat_cluster = zeros(size(new_germ,1));
-
-mat_cluster = new_germ(:,3) > 170;
-
-new_germ(:,6) = mat_cluster;
-
-for i=1:h
-    for j=1:w
-        mat_binarisation(i,j) = new_germ(matrix_label(i,j),6);
+    
+    K = size(germ,1);
+    % Gestion de la compacité (m = S par défaut)
+    m = S;
+    % coté du voisinage d'un germe
+    n_voisin = 3;
+    
+    %% Affinement de la grille
+    [Gx,Gy] = imgradient(rgb2gray(im(:,:,:,n)));
+    grad = (double(Gx)+double(Gy))/2;
+    
+    for i = 1:size(germ,1)
+        x_germ = germ(i,1);
+        y_germ = germ(i,2);
+        x_min = max(1,x_germ-1);
+        x_max = min(h,x_germ+1);
+        y_min = max(1,y_germ-1);
+        y_max = min(w,y_germ+1);
+        % Voisinage n du germe
+        voisinage = Gy(x_min:x_max,y_min:y_max);
+        % Valeur minimale dans le voisinage
+        [valeur,indice]=min(voisinage(:));
+        [indX,indY] = ind2sub(size(voisinage),indice);
+        x_newgerm = floor(x_min+(indX-1));
+        y_newgerm = floor(y_min+(indY-1));
+        % Mise à jour du germe
+        germ(i,1:2) = [x_newgerm y_newgerm];
+        germ(i,3:5) = im_n(x_newgerm,y_newgerm,1:3);
     end
-end
+    
+    %% Kmeans
+    % Transformation de l'image au format des germes et compacité
+    im_kmeans = zeros(h*w,5);
+    ligne=1;
+    for x=1:h
+        for y=1:w
+            im_kmeans(ligne,:)=[x*(m/S) y*(m/S) im_n(x,y,1) im_n(x,y,2) im_n(x,y,3)];
+            ligne=ligne+1;
+        end
+    end
+    % Compacité sur les germes
+    for ligne=1:K
+       germ(ligne,1)=germ(ligne,1)*(m/S);
+       germ(ligne,2)=germ(ligne,2)*(m/S);
+    end
+    [res_kmeans, new_germ] = kmeans(im_kmeans,K,'Start',germ,'EmptyAction','drop');
+    % Annulation de la compacité sur les germes résultants
+    for ligne=1:size(new_germ,1)
+       new_germ(ligne,1)=new_germ(ligne,1)*(S/m);
+       new_germ(ligne,2)=new_germ(ligne,2)*(S/m);
+    end
+    matrix_label = (reshape(res_kmeans,size(im_n,2),size(im_n,1)))';
+    mask = boundarymask(matrix_label);
+    %% Amélioration de la connexité
+    
+    
+    %% Binarisation
+    mat_binarisation = zeros(size(im_n(:,:,1)));
+    mat_cluster = new_germ(:,3) > new_germ(:,5)+3;
+    new_germ(:,6) = mat_cluster;
+    
+    for i=1:h
+        for j=1:w
+            mat_binarisation(i,j) = new_germ(matrix_label(i,j),6);
+        end
+    end
 
-figure;
-imshow(mat_binarisation)
+    figure;
+    imshow(labeloverlay(im(:,:,:,n_calc),mask,'Transparency',0));
+    figure;
+    imshow(mat_binarisation);
+end
